ef21940c782bdbbe0d8ead147ffb359169574d3e
Andrew Listopadov <andrew.listopadov@gmail.com>
2018-01-27 16:24:39 +0300

    added hero detail component

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -8,8 +8,8 @@ import { HEROES } from '../mock-heroes';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-  selectedHero: Hero;
   heroes: Hero[];
+  selectedHero: Hero;
 
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
